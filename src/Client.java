7a1e8ae064155c9c27434d448eba63d9abc931ea
sui_h <78638687+He-Sui@users.noreply.github.com>
2022-04-06 21:26:52 +0800

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -5,12 +5,14 @@ public class Client {
             DataManipulation dm = new DataFactory().createDataManipulation(args[0]);
             dm.openDatasource();
             dm.createTable();
+            long begin = System.currentTimeMillis();
             dm.importData();
+            long end = System.currentTimeMillis();
+            System.out.println(end - begin);
             dm.closeDatasource();
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
-
     }
 }
 
